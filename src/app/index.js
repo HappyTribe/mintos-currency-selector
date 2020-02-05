29e1818fb57a7e2e764fd7a423910a5a7ede9955
Mārtiņš Barkovs <martins.barkovs@tet.lv>
2020-02-05 19:40:34 +0200

    Create a default state array for currencies variable + setup main application layout

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,34 @@
 import { hot } from 'react-hot-loader'
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => <div>Hello App</div>
+import { CurrencyList } from './components/index'
+import { initialCurrencies } from './state'
+
+import './styles.scss'
+
+const App = () => {
+    const [currencies, setCurrencies] = useState(initialCurrencies)
+
+    const onUpdateCurrencies = ({ key, isActive }) => {
+        const newCurrencies = currencies.slice()
+        newCurrencies.find(currency => currency.key === key).isActive = isActive
+        setCurrencies(newCurrencies)
+    }
+
+    return (
+        <div className="container">
+            <CurrencyList
+                currencies={currencies.filter(currency => currency.isActive)}
+                listType="active"
+                onUpdateCurrencies={onUpdateCurrencies}
+            />
+            <CurrencyList
+                currencies={currencies}
+                listType="all"
+                onUpdateCurrencies={onUpdateCurrencies}
+            />
+        </div>
+    )
+}
 
 export default hot(module)(App)
---
+++ src/app/state.js
@@ -0,0 +1,13 @@
+const initialCurrencies = [
+    { key: 'EUR', isActive: true },
+    { key: 'PLN', isActive: true },
+    { key: 'GEL', isActive: false },
+    { key: 'DKK', isActive: false },
+    { key: 'CZK', isActive: false },
+    { key: 'GBP', isActive: false },
+    { key: 'SEK', isActive: false },
+    { key: 'USD', isActive: false },
+    { key: 'RUB', isActive: false },
+]
+
+export { initialCurrencies }
